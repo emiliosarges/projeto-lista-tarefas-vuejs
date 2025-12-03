29a0addb76383c97f30a593ea9f0b8235c1b3413
emiliosargesdev <emiliosarges@gmail.com>
2025-12-02 23:39:14 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,50 @@
+const tarefas = []
+
+const ToDoList = {
+    data() {
+        return {
+            tarefas,
+            novaTarefa: {
+                nome: null,
+                feita: false
+            }
+        }
+    },
+    methods: {
+        adicionarTarefa: function() {
+            if(this.novaTarefa.nome) {
+                this.tarefas.push(this.novaTarefa);
+                this.novaTarefa = {
+                    feito: false
+                };
+                localStorage.setItem("tarefas", JSON.stringify(this.tarefas));
+            } else {
+                alert("Digite uma tarefa para adicionar")
+            }
+            
+           
+        },
+
+        limparTudo: function() {
+            if(!(this.tarefas.length === 0)){
+                confirm("Suas tarefas serão apagadas definitivamente")
+                this.tarefas = []
+            } else {
+                alert("Não há tarefas para apagar")
+            }
+            
+        },
+        storeTarefas() {
+            localStorage.setItem("tarefas", JSON.stringify(this.tarefas));
+            console.log("updated")
+        }
+    },
+    created() {
+        this.tarefas = localStorage.getItem("tarefas") ? JSON.parse(localStorage.getItem("tarefas")) : this.tarefas;
+    },
+    updated() { 
+        // this.tarefas = localStorage.getItem("tarefas") ? JSON.parse(localStorage.getItem("tarefas")) : this.tarefas;
+    } //Outra maneira de fazer, melhor para pequenas aplicações como essa.
+}
+
+Vue.createApp(ToDoList).mount('#app');
